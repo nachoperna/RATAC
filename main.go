7927ad62b067c563d4e2752b8c68b45c3778b824
nachoperna <ignacioperna@gmail.com>
2026-04-22 00:39:31 -0300

    conexion con la base y repositorio de pacientes lista

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	sqlc "RATAC/DB/sqlc"
+	"RATAC/application"
+	"RATAC/domain"
+	dbrepo "RATAC/infrastructure/DB"
+	ui "RATAC/infrastructure/UI"
+	"database/sql"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const db_conection = "host=localhost port=5432 user=admin password=password dbname=RATAC_DB sslmode=disable"
+const port = ":8080"
+
+func main() {
+	db, err := sql.Open("postgres", db_conection)
+	if err != nil {
+		log.Fatalf("Error al conectar con la Base de Datos: %v", err)
+	}
+	defer db.Close()
+
+	queries := sqlc.New(db)
+
+	var pacienteRepo domain.PacienteRepository = dbrepo.NewPacienteRepository(queries)
+	pacienteServices := application.NewPacienteService(pacienteRepo)
+	pacienteHandler := ui.NewPacienteHandler(pacienteServices)
+
+	http.HandleFunc("/", pacienteHandler.ListPacientes)
+	http.ListenAndServe(port, nil)
+}
