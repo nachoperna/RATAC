ecb368b045419a8c29e7719151336424f883afcd
nachoperna <ignacioperna@gmail.com>
2026-04-25 11:54:08 -0300

    querie insert gradooncologico

--- DB/queries/grado_oncologico.sql
+++ DB/queries/grado_oncologico.sql
@@ -1,3 +1,12 @@
+-- name: CreateGradoOncologico :one
+INSERT INTO Grado_oncologico(
+      Caracteristica, Muestra_analizada, Puntaje, 
+      Descripciones_microscopicas_Descripcion, Descripciones_microscopicas_Pacientes_Protocolo
+) VALUES (
+      $1, $2, $3, $4, $5
+)
+RETURNING *;
+
 -- name: GetGradoOncologico :one
 SELECT * FROM Grado_oncologico
 WHERE Descripciones_microscopicas_Pacientes_Protocolo = $1
@@ -8,8 +17,7 @@ UPDATE Grado_oncologico
 SET 
     Caracteristica = $3,
     Muestra_analizada = $4,
-    Puntaje = $5,
-    Total = $6
+    Puntaje = $5
 WHERE Descripciones_microscopicas_Pacientes_Protocolo = $1
 AND Descripciones_microscopicas_Descripcion = $2
 RETURNING *;
