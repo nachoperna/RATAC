caf0b3624715bce5e4b032a115ebc81ee9a3b837
nachoperna <ignacioperna@gmail.com>
2026-04-25 11:59:37 -0300

    fix: puntero por instancia

--- domain/diagnostico.go
+++ domain/diagnostico.go
@@ -9,7 +9,7 @@ type Diagnostico struct {
 
 type DiagnosticoRepository interface {
 	// Necesita saber a qué descripción y protocolo asociar el diagnóstico y sus imágenes
-	CreateDiagnostico(ctx context.Context, protocolo string, descripcionMicro string, diagnostico *Diagnostico) error
+	CreateDiagnostico(ctx context.Context, protocolo string, descripcionMicro string, diagnostico Diagnostico) error
 
 	// Recupera el diagnóstico asociado a una descripción específica
 	GetDiagnostico(ctx context.Context, protocolo string, descripcionMicro string) (*Diagnostico, error)
