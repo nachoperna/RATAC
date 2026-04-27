91f60f8f1b1a1c5a7d2079d406ec0a4cba6b1ef0
tomas-padilla <tpadilla@alumnos.exa.unicen.edu.ar>
2026-04-27 16:17:21 -0300

    Agrego tabla de log,trigger y function. (Sin probar)

--- DB/schema/schema.sql
+++ DB/schema/schema.sql
@@ -43,6 +43,51 @@ CREATE TABLE Grado_oncologico (
     CONSTRAINT Grado_oncologico_pk PRIMARY KEY (id,Descripciones_microscopicas_Pacientes_Protocolo,Descripciones_microscopicas_Descripcion)
 );
 
+CREATE TABLE historial_acciones (
+    id serial PRIMARY KEY,
+    usuario varchar(50) NOT NULL DEFAULT 'sistema',
+    accion varchar(255) NOT NULL,
+    tabla_afectada varchar(50) NOT NULL,
+    fecha timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    datos_anteriores text,
+    datos_nuevos text
+);
+
+
+-- Funciones y triggers para registrar el historial de acciones
+
+CREATE OR REPLACE FUNCTION registrar_historial()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (TG_OP = 'DELETE') THEN
+        -- Si borramos, solo guardamos lo que se eliminó (OLD)
+        INSERT INTO historial_acciones (accion, tabla_afectada, datos_viejos)
+        VALUES (TG_OP, TG_TABLE_NAME, row_to_json(OLD)::TEXT);
+        RETURN OLD;
+        
+    ELSIF (TG_OP = 'UPDATE') THEN
+        -- Si actualizamos, guardamos cómo estaba (OLD) y cómo quedó (NEW)
+        INSERT INTO historial_acciones (accion, tabla_afectada, datos_viejos, datos_nuevos)
+        VALUES (TG_OP, TG_TABLE_NAME, row_to_json(OLD)::TEXT, row_to_json(NEW)::TEXT);
+        RETURN NEW;
+        
+    ELSIF (TG_OP = 'INSERT') THEN
+        -- Si insertamos, solo guardamos lo nuevo (NEW)
+        INSERT INTO historial_acciones (accion, tabla_afectada, datos_nuevos)
+        VALUES (TG_OP, TG_TABLE_NAME, row_to_json(NEW)::TEXT);
+        RETURN NEW;
+        
+    END IF;
+    
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_historial_diagnosticos
+AFTER INSERT OR UPDATE OR DELETE ON Pacientes, descripciones_microscopicas, Imagenes, Grado_oncologico
+FOR EACH ROW
+EXECUTE FUNCTION registrar_historial();
+
 -- foreign keys
 -- Reference: Descripciones_microscopicas_Pacientes (table: Descripciones_microscopicas)
 ALTER TABLE Descripciones_microscopicas ADD CONSTRAINT Descripciones_microscopicas_Pacientes
